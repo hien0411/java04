f8523a8e9f59df2378d1503269090fa06e510218
hien0411 <caigicungduochhhh@gmail.com>
2020-08-06 08:50:22 +0700

    second cm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("lop jav jo4");
+        System.out.println("aaaaaaaaaa");
     }
 }
