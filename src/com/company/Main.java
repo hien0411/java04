29c191acaaf3705d7353d20c8c8a6024633a0fbb
hien0411 <caigicungduochhhh@gmail.com>
2020-08-06 15:28:33 +0700

    BTVN-buoi1-bai4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,19 @@ public class Main {
     public static void main(String[] args) {
         int a;
         int k;
-        int max = 0;
+        int min = 10;
+        int dem = 0;
         System.out.println("nhap so a:");
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
         while (a>0){
             k =  a % 10;
             a /= 10;
-            if(k > max){
-                max = k;
+            if(k < min){
+                min = k;
+                dem += 1;
             }
         }
-        System.out.println("max = " + max);
+        System.out.println("so luong chu so nho nhat la: " + dem);
     }
 }
