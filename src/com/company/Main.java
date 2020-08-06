5665fc09af4523dc464003a3f951af9cd46e4629
hien0411 <caigicungduochhhh@gmail.com>
2020-08-06 09:03:18 +0700

    third cm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("nhap");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println("number"+ number);
+        System.out.println("check 1");
+
     }
 }
