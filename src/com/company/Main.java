034868d962527fd6f5a24df3975840a4c10e4366
hien0411 <caigicungduochhhh@gmail.com>
2020-08-06 13:45:05 +0700

    BTVN-buoi1-bai2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("lop jav jo4");
-        System.out.println("aaaaaaaaaa");
+        int k;
+        int tong = 0;
+        System.out.println("nhap so a:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        while (a != 0) {
+            k = a % 10;
+            if (k % 2 != 0) {
+                tong += k;
+            }
+            a /= 10;
+        }
+        System.out.println(" tong = " + tong);
     }
 }
