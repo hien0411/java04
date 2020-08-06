96b4db97ceb535a040547819adc7de520994fe8c
hien0411 <caigicungduochhhh@gmail.com>
2020-08-06 10:25:33 +0700

    BTVN-buoi1-bai2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         int k;
-        int tong = 0;
+        int dem = 0;
         System.out.println("nhap so a:");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         while (a!=0){
             k = a % 10;
-            if(k % 2 == 0){
-                tong += k;
+            if(k % 2 != 0){
+                dem += 1;
             }
             a /= 10;
         }
-        System.out.println("tong = " + tong);
+        System.out.println("so luong so le =  " + dem);
     }
 }
